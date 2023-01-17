d0804c144745aa951ab68262cbaf6e95827cb58d
Raunak-github <96681392+Raunak-github@users.noreply.github.com>
2023-01-17 20:05:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function con()
+{
+    var FN ="Raunak"
+    var LN ="singh"
+    console.log(FN)
+    console.log(LN)
+    console.log(FN.concat(LN))
+    var N1 ="8"
+    var N2 ="6"
+    let SY =["+","-","||"]
+    console.log(N1)
+    console.log(SY[0])
+    console.log(N2)
+    console.log(SY[2])
+    console.log(N1+N2)
+    console.log(N1)
+    console.log(SY[1])
+    console.log(N2)
+    console.log(SY[2])
+    console.log(N1-N2)
+}
